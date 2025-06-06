d5ca393c379abfc0b55f41bf344e6a22f76a1449
Harvy Jones Pontillas <harvyjonespontillas@gmail.com>
2025-06-06 23:00:38 +0800

    predict route

--- api/main.py
+++ api/main.py
@@ -32,3 +32,29 @@ class MatchResponse(BaseModel):
     is_match: bool
     threshol: float = 0.8
 
+@app.post("/predict_match", response_model=MatchResponse)
+def predict_match(request: MatchRequest):
+    try:
+        # Step 1: Preprocess
+        name1 = standardize_name(request.name1)
+        name2 = standardize_name(request.name2)
+
+        # Step 2: Feature Engineering
+        features = compute_features(name1, name2)
+        df_features = pd.DataFrame([features])
+
+        # Step 3: Predict
+        prob = model.predict_proba(df_features)[:, 1][0]
+        threshold = 0.5
+        is_match = prob >= threshold
+
+        # Step 4: Log
+        logging.info(f"{request.name1}, {request.name2}, {prob:.4f},{is_match}")
+
+        return MatchResponse(
+            match_probability=round(prob, 4),
+            is_match=is_match,
+            threshold=threshold
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
